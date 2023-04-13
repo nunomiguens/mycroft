fbd339e788b387f64151c720c0633500efbb7342
nmiguens <nuno-filipe.miguens_external@capgemini.com>
2023-04-13 05:01:02 -0500

    Initial commit

--- __init__.py
+++ __init__.py
@@ -4,6 +4,17 @@ from mycroft import MycroftSkill, intent_file_handler
 class IceCreamShop(MycroftSkill):
     def __init__(self):
         MycroftSkill.__init__(self)
+        
+    # »»»»»»»»»»»»»»»  Logging info
+    def initialize(self):
+        self.log.info("the skill has loaded huzza!")
+        self.log.warning("Something is slightly off")
+        self.log.error("Oh noes! Something is broken")
+        try:
+            print(new_var)
+        except NameError:
+            self.log.exception("the code broke!")
+
 
     @intent_file_handler('shop.cream.ice.intent')
     def handle_shop_cream_ice(self, message):
@@ -16,4 +27,11 @@ class IceCreamShop(MycroftSkill):
 
 def create_skill():
     return IceCreamShop()
+    
+    
+"""
+»»»» 
+
+
+"""
 
